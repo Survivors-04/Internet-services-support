1ca6ee457ad1b78d22bf15de18ac3b4a46c7e473
Enrique <enrique.barbosasilva@gmail.com>
2022-11-01 17:58:13 -0300

    feat: deleteClient-service-created

---
+++ src/services/client/deleteClient.service.ts
@@ -0,0 +1,30 @@
+import { AppDataSource } from "../../data-source";
+import { Client } from "../../entities/client.entity";
+import { AppError } from "../../errors/appError";
+
+const deleteClientService = async (id: string) => {
+  const clientRepository = AppDataSource.getRepository(Client);
+
+  const client = await clientRepository.findOneBy({
+    id,
+  });
+
+  if (!client) {
+    throw new AppError("Client not found", 404);
+  }
+
+  if (!client?.isActive) {
+    throw new AppError("This account are disabled", 400);
+  }
+
+  client.isActive = false;
+  await clientRepository.save(client);
+
+  const returningClient = await clientRepository.findOneBy({
+    id,
+  });
+
+  return returningClient!;
+};
+
+export default deleteClientService;
