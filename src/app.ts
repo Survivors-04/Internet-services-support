f743d608f34faf5192cfb8d298266d6fcdddafd8
Enrique <enrique.barbosasilva@gmail.com>
2022-11-01 13:57:20 -0300

    refactor: code-changed

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/HandleError.middleware";
+import clientRoutes from "./routes/client.routes";
 
 const app = express();
 app.use(express.json());
 
 export default app;
 
+app.use("/clients", clientRoutes);
 app.use(handleErrorMiddleware);
 
 app.listen(3000, () => {
