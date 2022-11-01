cd614413132d12581466ac5b548daf399815a80e
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-11-01 17:20:08 -0300

    feat: test-update

---
+++ src/interfaces/collaborator/index.ts
@@ -0,0 +1,8 @@
+export interface ICollaborator  {
+    name: string,
+    cpf: string,
+    telephone: string,
+    email: string,
+    password: string,
+    is_Active: true,
+  };
--- src/tests/mocks/index.ts
+++ src/tests/mocks/index.ts
@@ -1,10 +1,28 @@
+
+import { ICollaborator } from "../../interfaces/collaborator";
+import { IInternetPlan } from "../../interfaces/internetPlan";
 import { ISupervisorsRequest } from "../../interfaces/supervisors";
 
 export const mockedSupervisor: ISupervisorsRequest = {
-    name: "Teste",
-    email: "teste@mail.com",
-    password: "Teste123",
-    cpf: 12345678901,
-    telephone: 13984512783,
-    is_manager: false
-}
+  name: "Teste",
+  email: "teste@mail.com",
+  password: "Teste123",
+  cpf: 12345678901,
+  telephone: 13984512783,
+  is_manager: false,
+};
+
+export const mockedInternetPlans: IInternetPlan = {
+  name: "Teste",
+  description: "Teste123",
+  price: 400.0,
+};
+
+export const mockedCollaborator:ICollaborator = {
+  name: "Teste",
+  cpf: "12345678901",
+  telephone: "13984512783",
+  email: "teste@mail.com",
+  password: "Teste123",
+  is_Active: true
+};
