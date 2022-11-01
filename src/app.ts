fce838d9ec19a6eb5693424e84e3b3f9c3afd688
mazuquiebe-filho <mazuquiebe75@gamil.com>
2022-11-01 15:43:59 -0300

    feat: import and use collaboratorRouter

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/HandleError.middleware";
-
+import collaboratorRouter from "./routes/colaborator";
 const app = express();
 app.use(express.json());
 
 export default app;
 
 app.use(handleErrorMiddleware);
+app.use('/collaborators', collaboratorRouter);
 
 app.listen(3000, () => {
   console.log("Server is running");
