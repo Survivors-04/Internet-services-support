2e6c1e10e90602959ad152040d2a60a291fb12b3
Enrique <enrique.barbosasilva@gmail.com>
2022-11-01 16:15:23 -0300

    feat: updateClient-route-created

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import createClientController from "../controllers/client/createClient.controller";
 import listClientByIdController from "../controllers/client/listClientById.controller";
 import listClientController from "../controllers/client/listClients.controller";
+import updateClientController from "../controllers/client/updateClient.controller";
 
 const routes = Router();
 
 routes.post("", createClientController);
 routes.get("", listClientController);
 routes.get("/:id", listClientByIdController);
+routes.patch("/:id", updateClientController);
 
 export default routes;
