485f5d2be31a0c82d3352925ae53577e83b800fa
Enrique <enrique.barbosasilva@gmail.com>
2022-11-01 17:28:37 -0300

    refactor: code-changed

---
+++ src/routes/colaborator.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import createCollaboratorController from "../controllers/collaborators/createColaborator.controller";
+import getCollaboratorsDataController from "../controllers/collaborators/getColaboratorData.controller";
+import updateCollaboratorController from "../controllers/collaborators/updateColaborator.controller";
+import deleteCollaboratorController from "../controllers/collaborators/deleteColaborator.controller";
+
+const collaboratorRouter = Router();
+
+collaboratorRouter.post("", createCollaboratorController);
+collaboratorRouter.get("", getCollaboratorsDataController);
+collaboratorRouter.get("/:id", getCollaboratorsDataController);
+collaboratorRouter.patch("/:id", updateCollaboratorController);
+collaboratorRouter.delete("/:id", deleteCollaboratorController);
+
+export default collaboratorRouter;
