283913cb0a97591e5003a713d5154e073ad7199b
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-11-01 12:56:38 -0300

    feat: tests-supervisors

--- src/tests/integration/supervisors/supervisorsRoutes.test.ts
+++ src/tests/integration/supervisors/supervisorsRoutes.test.ts
@@ -1,7 +1,10 @@
 import { DataSource } from "typeorm";
 import { AppDataSource } from "../../../data-source";
+import request from "supertest";
+import app from "../../../app";
+import { mockedSupervisor } from "../../mocks";
 
-describe("Create an Supervisor", () => {
+describe("/supervisors", () => {
   let connection: DataSource;
 
   beforeAll(async () => {
@@ -16,5 +19,35 @@ describe("Create an Supervisor", () => {
     await connection.destroy();
   });
 
-  test("Should insert the information of new user in the database", async () => {});
+  test("POST /supervisors - Should insert the information of new user in the database", async () => {
+    const { body, status } = await request(app)
+      .post("/supervisors")
+      .send(mockedSupervisor);
+    
+    expect(body).toHaveProperty("id");
+    expect(body).toHaveProperty("name");
+    expect(body).toHaveProperty("email");
+    expect(body).toHaveProperty("is_manager");
+    expect(body).toHaveProperty("cpf");
+    expect(body).toHaveProperty("telephone");
+    expect(body).not.toHaveProperty("password");
+    expect(body.name).toEqual("Teste");
+    expect(body.email).toEqual("teste@mail.com");
+    expect(body.cpf).toEqual("12345678901");
+    expect(body.telephone).toEqual(13984512783);
+    expect(body.is_manager).toEqual(false);
+    expect(status).toBe(201);
+  });
+
+  test("POST /supervisors - Should not be able to create a supervisor that already exists", async () => {
+    const { body, status } = await request(app)
+      .post("/supervisors")
+      .send(mockedSupervisor);
+
+    expect(body).toHaveProperty("message");
+    expect(status).toBe(400);
+  });
+
+  test('')
+
 });
