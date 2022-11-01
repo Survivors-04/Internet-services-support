c4fe55b58fef84a2b658e0f35e92e6853224419a
Enrique <enrique.barbosasilva@gmail.com>
2022-11-01 17:28:27 -0300

    refactor: code-changed

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,16 @@ import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/HandleError.middleware";
-import collaboratorRouter from "./routes/colaborator";
+import collaboratorRouter from "./routes/colaborator.routes";
 import clientRoutes from "./routes/client.routes";
 
 const app = express();
 app.use(express.json());
 
-
-
 app.use("/clients", clientRoutes);
 
 app.use(handleErrorMiddleware);
-app.use('/collaborators', collaboratorRouter);
+app.use("/collaborators", collaboratorRouter);
 
 app.listen(3000, () => {
   console.log("Server is running");
