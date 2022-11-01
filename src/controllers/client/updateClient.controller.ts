bf94764f37388c92bc2aa3437be8ff364b4bf57d
Enrique <enrique.barbosasilva@gmail.com>
2022-11-01 16:14:46 -0300

    feat: updateClient-controller-created

---
+++ src/controllers/client/updateClient.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { IClientUpdate } from "../../interfaces/clients";
+import updateClientService from "../../services/client/updateClient.service";
+
+const updateClientController = async (req: Request, res: Response) => {
+  const client: IClientUpdate = req.body;
+  const id: string = req.params.id;
+
+  const updatedClient = await updateClientService(client, id);
+
+  return res.status(200).json({
+    message: "Client updated",
+    user: updatedClient,
+  });
+};
+
+export default updateClientController;
