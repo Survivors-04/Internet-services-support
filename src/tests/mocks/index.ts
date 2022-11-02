091d334b4b16af21033aa02031c0f2bfb79c8362
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-11-02 18:02:43 -0300

    fix: mocks-merge

--- src/tests/mocks/index.ts
+++ src/tests/mocks/index.ts
@@ -4,7 +4,6 @@ import { IInternetPlan } from "../../interfaces/internetPlan";
 import { ISupervisorsRequest } from "../../interfaces/supervisors";
 
 export const mockedSupervisor: ISupervisorsRequest = {
-<<<<<<< HEAD
   name: "Teste",
   email: "teste@mail.com",
   password: "Teste123",
@@ -27,14 +26,6 @@ export const mockedCollaborator:ICollaborator = {
   password: "Teste123",
   is_Active: true
 };
-=======
-    name: "Teste",
-    email: "teste@mail.com",
-    password: "Teste123",
-    cpf: 12345678901,
-    telephone: 13984512783,
-    is_manager: false
-}
 
 export const mockedAttendance = {
   collaborator_id: "",
@@ -42,4 +33,4 @@ export const mockedAttendance = {
   service_id: "",
 };
 
->>>>>>> 57a2f427dd32b0e04cb022f279134b3b70fb2797
+
