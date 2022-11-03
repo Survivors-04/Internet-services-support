87eb8aacd51c4a415e18ebd6e42d6edbba37fe3b
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-11-03 14:09:52 -0300

    feat: tests-supervisors

--- src/tests/integration/supervisors/supervisorsRoutes.test.ts
+++ src/tests/integration/supervisors/supervisorsRoutes.test.ts
@@ -67,7 +67,7 @@ describe("/supervisors", () => {
     expect(status).toBe(403);
   });
 
-  test("GET /supevisors - Must be able to list supervisors", async () => {
+  test("GET /supevisors - Must be able to list all supervisors", async () => {
     await request(app).post("/supervisors").send(mockedManager);
     const managerLogin = await request(app)
       .post("/login")
@@ -95,7 +95,7 @@ describe("/supervisors", () => {
     expect(status).toBe(403);
   });
 
-  test("PATCH /supervisors/:id - Must be able to update supervisor", async () => {
+  test("PATCH /supervisors/:id - Must be able to update a supervisor", async () => {
     const newValues = { name: "Joana Brito", email: "joanabrito@mail.com" };
 
     const managerLogin = await request(app)
@@ -231,5 +231,86 @@ describe("/supervisors", () => {
 
     expect(response.body).toHaveProperty("message");
     expect(response.status).toBe(401);
+  });
+
+  test("DELETE /supervisors/:id - Must be able to soft delete a supervisor", async () => {
+    await request(app).post("/supervisors").send(mockedManager);
+
+    const managerLogin = await request(app)
+      .post("/login")
+      .send(mockedManagerLogin);
+    const token = `Bearer ${managerLogin.body.token}`;
+
+    const deletedSupervisor = await request(app)
+      .get("/supervisors")
+      .set("Authorization", token);
+    const deletedSupervisorId = deletedSupervisor.body[0].id;
+
+    const { status } = await request(app)
+      .delete(`/supervisors/${deletedSupervisorId}`)
+      .set("Autorization", token);
+    const { body } = await request(app)
+      .get("/supervisors")
+      .set("Authorization", token);
+
+    expect(status).toBe(204);
+    expect(body[0].is_active).toBe(false);
+  });
+
+  test("DELETE /supervisors/:id - Should not be able to delete a supervisor without manager permission", async () => {
+    const managerLogin = await request(app)
+      .post("/login")
+      .send(mockedManagerLogin);
+    const token = `Bearer ${managerLogin.body.token}`;
+
+    const deletedSupervisor = await request(app)
+      .get("/supervisors")
+      .set("Authorization", token);
+    const deletedSupervisorId = deletedSupervisor.body[0].id;
+    const deletedSupervisorToken = deletedSupervisor.body.token;
+
+    const { body, status } = await request(app)
+      .delete(`/supervisors/${deletedSupervisorId}`)
+      .set("Autorization", deletedSupervisorToken);
+
+    expect(body).toHaveProperty("message");
+    expect(status).toBe(403);
+  });
+
+  test("DELETE /supervisors/:id -  shouldn't be able to delete user with is_active = false", async () => {
+    await request(app).post("/users").send(mockedManager);
+
+    const managerLogin = await request(app)
+      .post("/login")
+      .send(mockedManagerLogin);
+    const token = `Bearer ${managerLogin.body.token}`;
+
+    const deletedSupervisor = await request(app)
+      .get("/supervisors")
+      .set("Authorization", token);
+    const deletedSupervisorId = deletedSupervisor.body[0].id;
+
+    const { body, status } = await request(app)
+      .delete(`/supervisors/${deletedSupervisorId}`)
+      .set("Authorization", token);
+
+    expect(status).toBe(400);
+    expect(body).toHaveProperty("message");
+  });
+
+  test("DELETE -  should not be able to delete user with invalid id", async () => {
+    await request(app).post("/users").send(mockedManager);
+
+    const managerLogin = await request(app)
+      .post("/login")
+      .send(mockedManagerLogin);
+    const token = managerLogin.body.token;
+
+    const response = await request(app)
+      .delete(`/users/13970660-5dbe-423a-9a9d-5c23b37943cf`)
+      .set("Authorization", token);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("message");
   });
 });
