829ccabcb3b9b036736716db7b800e74cee0b17d
mazuquiebe-filho <mazuquiebe75@gamil.com>
2022-11-04 10:13:44 -0300

    feat: app import-attendances-routes

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/HandleError.middleware";
 import collaboratorRouter from "./routes/colaborator.routes";
 import clientRoutes from "./routes/client.routes";
+import attendancesRouter from "./routes/attendances.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/clients", clientRoutes);
 app.use("/collaborators", collaboratorRouter);
+app.use("/attendances", attendancesRouter);
 app.use(handleErrorMiddleware);
 
 
