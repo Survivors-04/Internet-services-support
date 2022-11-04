433c090f6ef8aebd2ed97d91315c7e3e1f4d601e
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-11-04 15:31:36 -0300

    fix: data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,7 +13,7 @@ const AppDataSource = new DataSource(
         type: "postgres",
         url: process.env.DATABASE_URL,
         ssl:
-          process.env.NODE_END === "production"
+          process.env.NODE_ENV === "production"
             ? { rejectUnauthorized: false }
             : false,
         logging: true,
