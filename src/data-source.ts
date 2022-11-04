7836594ecd8c6e905432fc81e2387996ab3ca2dc
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-11-04 16:33:10 -0300

    fix: data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -21,11 +21,11 @@ const AppDataSource = new DataSource(
 
         entities:
           process.env.NODE_ENV === "production"
-            ? ["dist/src/entities/*.js"]
+            ? ["dist/entities/*.js"]
             : ["src/entities/*.ts"],
         migrations:
           process.env.NODE_ENV === "production"
-            ? ["dist/src/migrations/*.js"]
+            ? ["dist/migrations/*.js"]
             : ["src/migrations/*.ts"],
       }
 );
