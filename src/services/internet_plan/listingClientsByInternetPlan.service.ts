a8123d1853a80e6505a25a4ead2cabcb5e33adc0
Gabriel Oliveira <gboliveira2503@gmail.com>
2022-11-08 18:24:41 -0300

    refactor:listingClientsByInternetPlan

--- src/services/internet_plan/listingClientsByInternetPlan.service.ts
+++ src/services/internet_plan/listingClientsByInternetPlan.service.ts
@@ -5,18 +5,16 @@ import { AppError } from "../../errors/appError";
 
 export const listingClientsByInternetPlanService = async (
   id: string
-): Promise<Client[]> => {
+): Promise<Internet_plan> => {
   const clientsRepository = AppDataSource.getRepository(Client);
   const internetPlanRepository = AppDataSource.getRepository(Internet_plan);
-
-  const internetPlan = await internetPlanRepository.findOneBy({ id });
-  const clients = await clientsRepository.find();
+  // const clients = await internetPlanRepository
+  const plans = await internetPlanRepository.find({
+    relations: { client_plan: true },
+  });
+  const internetPlan = plans.find((plan) => plan.id === id);
 
   if (!internetPlan) throw new AppError("internet plan not found", 409);
 
-  const returnedClients = clients.filter(
-    (elem) => elem.client_plan.filter((plan) => plan.id === id).length > 0
-  );
-    console.log(returnedClients)
-  return returnedClients;
+  return internetPlan;
 };
