edc6224ecca03f2553d02bcba70cb17271b32637
mazuquiebe-filho <mazuquiebe75@gamil.com>
2022-11-09 15:46:03 -0300

    fix: interfaces attendances

--- src/interfaces/attendances/index.ts
+++ src/interfaces/attendances/index.ts
@@ -4,8 +4,9 @@ export interface IAttendanceRequest {
   clientId: string;
   serviceId: string;
   date: Date;
-}
+};
 
 export interface IAttendanceResponse extends IAttendanceRequest{
   id: string;
+  is_active: boolean;
 };
