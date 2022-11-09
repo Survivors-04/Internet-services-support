8f7bbe7c7944c1dc6c984696a9336475421e6802
mazuquiebe-filho <mazuquiebe75@gamil.com>
2022-11-09 15:44:23 -0300

    fix: services attendances

--- src/services/attendances/createAttendance.service.ts
+++ src/services/attendances/createAttendance.service.ts
@@ -3,6 +3,7 @@ import { Attendance } from "../../entities/attendance.entity";
 import { Client } from "../../entities/client.entity";
 import { Collaborator } from "../../entities/collaborator.entity";
 import { Services } from "../../entities/services.entity";
+import { AppError } from "../../errors/appError";
 import { IAttendanceRequest, IAttendanceResponse } from "../../interfaces/attendances";
 
 export const createAttendanceService = async (
@@ -13,59 +14,32 @@ export const createAttendanceService = async (
   const collaborRepo   = AppDataSource.getRepository(Collaborator);
   const servicesRepo   = AppDataSource.getRepository(Services);
 
-  const { clientId, collaboratorId, date, serviceId } = data;
+  const { clientId, collaboratorId, serviceId } = data;
 
   const client =       await clientsRepo.findOneBy({ id: clientId });
   const collaborator = await collaborRepo.findOneBy({ id: collaboratorId });
   const service =      await servicesRepo.findOneBy({ id: serviceId });
- 
-  const newAttendance  = attendanceRepo.create({
-    client:{
-      id:           client?.id,
-      cpf:          client?.cpf,
-      name:         client?.name,
-      email:        client?.email,
-      is_active:    client?.is_active,
-      client_plan:  client?.client_plan,
-      telephone:    client?.telephone,
-      updated_date: client?.updated_date,
-      attendance:   client?.attendance,
-      created_date: client?.created_date, 
-    },
-    date: date,
-    service: {
-      id:          service?.id,
-      name:        service?.name,
-      description: service?.description
-    },
-    collaborator: {
-      id:          collaborator?.id,
-      name:        collaborator?.name,
-      email:       collaborator?.email,
-      cpf:         collaborator?.cpf,
-      team:        collaborator?.team,
-      telephone:   collaborator?.telephone
-    },
+  
+  if(!client) throw new AppError("Client id is invalid",404);
+  if(!collaborator) throw new AppError("Collaborator id is invalid",404);
+  if(!service) throw new AppError("Service id is invalid",404);
+
+  const newAttendance = attendanceRepo.create({
+    client: client, 
+    collaborator: collaborator, 
+    service: service
   });
 
-  
   await attendanceRepo.save(newAttendance);
   
-  const { 
-    collaborator: collab, 
-    service:      serv, 
-    client:       clnt, 
-    date:         dte, 
-    id, 
-  } = newAttendance;
-
   const attendanceResponse: IAttendanceResponse = {
-    id:             id,
-    date:           dte,
-    clientId:       clnt.id,
-    serviceId:      serv.id,
-    collaboratorId: collab.id
-  }
+    clientId:       newAttendance.client.id,
+    collaboratorId: newAttendance.collaborator.id,
+    serviceId:      newAttendance.service.id,
+    date:           newAttendance.date,
+    id:             newAttendance.id,
+    is_active:      newAttendance.is_active
+  };
 
   return attendanceResponse;
 };
--- src/services/attendances/listAttendances.service.ts
+++ src/services/attendances/listAttendances.service.ts
@@ -4,48 +4,30 @@ import { Collaborator } from "../../entities/collaborator.entity";
 import { AppError } from "../../errors/appError";
 import { IAttendanceResponse } from "../../interfaces/attendances";
 
-export const listAttendancesService = async ( id:string ):Promise<IAttendanceResponse | IAttendanceResponse[]>  =>{
+export const listAttendancesService = async ( id:string ) =>{
 
   const attendancesRepo = AppDataSource.getRepository(Attendance);
   const collaboratorsRepo = AppDataSource.getRepository(Collaborator);
 
-  const padronizeAttendanceResponse = (attendance:Attendance):IAttendanceResponse =>{
-
-    const {client, collaborator, date, id, service } = attendance;
-
-    const padronizedResponse:IAttendanceResponse = {
-      id:             id,
-      date:           date,
-      clientId:       client.id,
-      serviceId:      service.id,
-      collaboratorId: collaborator.id,
-
-    };
-
-    return padronizedResponse;
-  };
-
   if( id ){
   
-    const attendance   = await attendancesRepo.findOneBy({ id:id });
-    const collaborator = await collaboratorsRepo.findOneBy({ id:id });
+    const attendance   = await attendancesRepo.findOneBy({ id });
+    const collaborator = await collaboratorsRepo.findOneBy({ id });
+    
 
-    if ( attendance ) {
-      const att:IAttendanceResponse = padronizeAttendanceResponse(attendance);
-      return att;
-    };
+    if ( attendance ) return attendance;
 
-    if( collaborator ){ 
-      const { attendance } = collaborator;
-      const atts:IAttendanceResponse[] = attendance.map( att => padronizeAttendanceResponse(att) );
-      return atts;
-    };
+    // if( collaborator ){ 
+      // const { attendance } = collaborator;
+      // const atts:IAttendanceResponse[] = attendance.map( att => padronizeAttendanceResponse(att) );
+      // return atts;
+    // };
 
     throw new AppError("Attendance information not found!",404);
   };
 
   const attendances = await attendancesRepo.find();
-  const attdcs = attendances.map(attdc => padronizeAttendanceResponse(attdc)); 
+  
+    return attendances;
 
-  return attdcs;
 };
