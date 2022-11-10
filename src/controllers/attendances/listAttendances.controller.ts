f89cc244427559e7786b394c9a90402e5acf35fe
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-11-09 21:09:38 -0300

    fix: tests-attendances

--- src/controllers/attendances/listAttendances.controller.ts
+++ src/controllers/attendances/listAttendances.controller.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from 'express';
-import { listAttendancesService } from '../../services/attendances/listAttendances.service';
+import { instanceToPlain } from "class-transformer";
+import { Request, Response } from "express";
+import { listAttendancesService } from "../../services/attendances/listAttendances.service";
 
-export const listAttendancesController = async (req:Request, res:Response) => {
-
-  const { id } = req.params
+export const listAttendancesController = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
   const attendances = await listAttendancesService(id);
-  return res.status(200).json(attendances); 
-
+  return res.status(200).json(instanceToPlain(attendances));
 };
